5ff3f94688e906d98408bcd26e5448d042af5c75
VERNAGUT-Titouan-2326112-A <titouan.vernagut@etu.univ-amu.fr>
2026-02-03 15:15:54 +0100

    wip

--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\DocumentRepository;
+use App\Repository\VideoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,19 +11,23 @@ use Symfony\Component\Routing\Attribute\Route;
 class TeacherController extends AbstractController
 {
     private DocumentRepository $documentRepository;
+    private VideoRepository $videoRepository;
 
-    public function __construct(DocumentRepository $documentRepository)
+    public function __construct(DocumentRepository $documentRepository, VideoRepository $videoRepository)
     {
         $this->documentRepository = $documentRepository;
+        $this->videoRepository = $videoRepository;
     }
     #[Route('/teacher', name: 'app_teacher_home')]
     public function index(): Response
     {
         $pdfs = $this->documentRepository->findAll();
+        $videos = $this->videoRepository->findAll();
         // On pointe vers ton template existant
         return $this->render('User/Professor/index.html.twig', [
             'controller_name' => 'TeacherController',
             'pdfs' => $pdfs,
+            'videos' => $videos,
         ]);
     }
 }
