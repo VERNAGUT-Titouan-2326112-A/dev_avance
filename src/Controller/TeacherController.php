fd2d2d9285921fb91ddf0d9447181f35f4aa29a2
Manon VERHILLE <manon.verhille@etu.univ-amu.fr>
2026-02-09 09:11:08 +0100

    fix: resolve document push in platform

--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CourseRepository;
 use App\Repository\DocumentRepository;
 use App\Repository\VideoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,20 +13,24 @@ class TeacherController extends AbstractController
 {
     private DocumentRepository $documentRepository;
     private VideoRepository $videoRepository;
+    private CourseRepository $courseRepository;
 
-    public function __construct(DocumentRepository $documentRepository, VideoRepository $videoRepository)
+    public function __construct(DocumentRepository $documentRepository, VideoRepository $videoRepository, CourseRepository $courseRepository)
     {
         $this->documentRepository = $documentRepository;
         $this->videoRepository = $videoRepository;
+        $this->courseRepository = $courseRepository;
     }
     #[Route('/teacher', name: 'app_teacher_home')]
     public function index(): Response
     {
         $pdfs = $this->documentRepository->findAll();
         $videos = $this->videoRepository->findAll();
+        $courses = $this->courseRepository->findAll();
         // On pointe vers ton template existant
         return $this->render('User/Professor/index.html.twig', [
             'controller_name' => 'TeacherController',
+            'courses' => $courses,
             'pdfs' => $pdfs,
             'videos' => $videos,
         ]);
