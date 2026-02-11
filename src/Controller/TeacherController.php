d9c70646853a55cd1a854dc7feaeb3aecf23d76b
Manon VERHILLE <manon.verhille@etu.univ-amu.fr>
2026-02-11 18:58:23 +0100

    feat: add general quiz results

--- src/Controller/TeacherController.php
+++ src/Controller/TeacherController.php
@@ -6,6 +6,7 @@ use App\Repository\CourseRepository;
 use App\Repository\DocumentRepository;
 use App\Repository\QuizRepository;
 use App\Repository\VideoRepository;
+use App\Repository\QuizAttemptRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,29 +17,39 @@ class TeacherController extends AbstractController
     private VideoRepository $videoRepository;
     private CourseRepository $courseRepository;
     private QuizRepository $quizRepository;
+    private QuizAttemptRepository $quizAttemptRepository;
 
     public function __construct(
         DocumentRepository $documentRepository,
         VideoRepository $videoRepository,
         CourseRepository $courseRepository,
-        QuizRepository $quizRepository
+        QuizRepository $quizRepository,
+        QuizAttemptRepository $quizAttemptRepository
     )
     {
         $this->documentRepository = $documentRepository;
         $this->videoRepository = $videoRepository;
         $this->courseRepository = $courseRepository;
         $this->quizRepository = $quizRepository;
+        $this->quizAttemptRepository = $quizAttemptRepository;
     }
 
     #[Route('/teacher', name: 'app_teacher_home')]
     public function index(): Response
     {
+        // On récupère les données proprement
+        $courses = $this->courseRepository->findAll();
+        $pdfs = $this->documentRepository->findAll();
+        $videos = $this->videoRepository->findAll();
+        $quizzes = $this->quizRepository->findAll();
+        $results = $this->quizAttemptRepository->findAll();
+
         return $this->render('User/Professor/index.html.twig', [
-            'controller_name' => 'TeacherController',
-            'courses' => $this->courseRepository->findAll(),
-            'pdfs' => $this->documentRepository->findAll(),
-            'videos' => $this->videoRepository->findAll(),
-            'quizzes' => $this->quizRepository->findBy([], ['id' => 'DESC']),
+            'courses' => $courses,
+            'pdfs' => $pdfs,
+            'videos' => $videos,
+            'quizzes' => $quizzes,
+            'results' => $results,
         ]);
     }
 }
